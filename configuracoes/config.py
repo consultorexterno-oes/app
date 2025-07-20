1247df8350c962341a699b15db2fb8ea7ab15184
consultorexterno-oes <consultor.externo@oesgestao.com.br>
2025-07-20 10:22:43 -0300

    Add files via upload

---
+++ configuracoes/config.py
@@ -0,0 +1,5 @@
+NOME_ARQUIVO_PREVISTO = "Base_Revisoes_Cronograma.xlsx"
+NOME_ARQUIVO_REFINADO = "02_refinado_output.xlsx"
+COLUNAS_ID = ["Classificação", "Revisão", "CC", "Complexo", "Área", "Gerência", "Cenário"]
+COLUNAS_MESES = []  # Isso será preenchido dinamicamente com os nomes dos meses
+
